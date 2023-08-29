40c315bbe4d0d7d0e82bbaf471ebca6bb60d4770
cuiyuanyuan <cuiyuanyuan@senseauto.com>
2023-08-29 17:46:56 +0800

    补充wgs84->百度的转换方法

--- convert.py
+++ convert.py
@@ -26,10 +26,17 @@ def bd09towgs84(bd: GPSPoint) -> GPSPoint:
     return wgs84
 
 
+# 84转百度
+def wgs84tobd09(wgs84: GPSPoint) -> GPSPoint:
+    hx = wgs84togcj02(wgs84)
+    bd = gcj02tobd09(hx)
+    return bd
+
+
 # 百度转火星
-def bd09togcj02(Bd: GPSPoint) -> GPSPoint:
-    x = Bd.lng - 0.0065
-    y = Bd.lat - 0.006
+def bd09togcj02(bd: GPSPoint) -> GPSPoint:
+    x = bd.lng - 0.0065
+    y = bd.lat - 0.006
     z = math.sqrt(x * x + y * y) - 0.00002 * math.sin(y * x_pi)
     theta = math.atan2(y, x) - .000003 * math.cos(x * x_pi)
     hx = GPSPoint()
@@ -38,8 +45,20 @@ def bd09togcj02(Bd: GPSPoint) -> GPSPoint:
     return hx
 
 
-# //火星转84
+# 火星转百度
+def gcj02tobd09(hx: GPSPoint) -> GPSPoint:
+    z = math.sqrt(hx.lng * hx.lng + hx.lat * hx.lat) + 0.00002 * math.sin(hx.lat * x_pi)
+    theta = math.atan2(hx.lat, hx.lng) + 0.000003 * math.cos(hx.lng * x_pi)
+    bd = GPSPoint()
+    bd.lng = z * math.cos(theta) + 0.0065
+    bd.lat = z * math.sin(theta) + 0.006
+    return bd
+
+
+# 火星转84
 def gcj02towgs84(hx: GPSPoint) -> GPSPoint:
+    if outOfChina(hx):
+        return hx
     dlat = transformlat(hx.lng - 105.0, hx.lat - 35.0)
     dlng = transformlng(hx.lng - 105.0, hx.lat - 35.0)
     radlat = hx.lat / 180.0 * math.pi
@@ -54,6 +73,34 @@ def gcj02towgs84(hx: GPSPoint) -> GPSPoint:
     wgs84.lat = hx.lat * 2 - mglat
     wgs84.lng = hx.lng * 2 - mglng
     return wgs84
+
+
+# WGS84转火星
+def wgs84togcj02(wgs84: GPSPoint) -> GPSPoint:
+    if outOfChina(wgs84):
+        return wgs84
+    dlat = transformlat(wgs84.lng - 105.0, wgs84.lat - 35.0)
+    dlng = transformlng(wgs84.lng - 105.0, wgs84.lat - 35.0)
+    radlat = wgs84.lat / 180.0 * math.pi
+    magic = math.sin(radlat)
+    magic = 1 - ee * magic * magic
+    sqrtmagic = math.sqrt(magic)
+    dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * math.pi)
+    dlng = (dlng * 180.0) / (a / sqrtmagic * math.cos(radlat) * math.pi)
+    hx = GPSPoint()
+    hx.lat = wgs84.lat + dlat
+    hx.lng = wgs84.lng + dlng
+    return hx
+
+
+# /*辅助函数*/
+# //判断是否在国内
+def outOfChina(wgs84: GPSPoint) -> bool:
+    if wgs84.lng < 72.004 or wgs84.lng > 137.8347:
+        return True
+    if wgs84.lat < 0.8293 or wgs84.lat > 55.8271:
+        return True
+    return False
 
 
 # /*辅助函数*/
@@ -84,7 +131,7 @@ def transformlng(lng: float, lat: float) -> float:
     return ret
 
 
-def make_data(input: str, output: str):
+def make_bd_2_84_data(input: str, output: str):
     wgs84List = []
     with open(input) as f:
         lines = f.readlines()
@@ -104,12 +151,36 @@ def make_data(input: str, output: str):
     with open(output, mode="w+") as f:
         for item in wgs84List:
             obj = {"longitude": item[0], "latitude": item[1]}
-            f.write("- " + json.dumps(obj)+"\n")
-
-            # f.write(f"- latitude: {item[1]}\n")
-            # f.write(f"  longitude: {item[0]}\n")
-
+            # f.write("- " + json.dumps(obj)+"\n")
+            f.write(f"- latitude: {item[1]}\n")
+            f.write(f"  longitude: {item[0]}\n")
             # f.write(f"{{{item[0]},{item[1]}}},\n")
+
+
+def make_84_2_bd_data(input: str, output: str):
+    bdList = []
+    with open(input) as f:
+        lines = f.readlines()
+        wgs84List = []
+
+        for l in lines:
+            ss = l.strip().split(",")
+            if len(ss) != 2:
+                continue
+            lng = float(ss[0])
+            lat = float(ss[1])
+            wgs84List.append((lng, lat))
+            # wgs84
+            wgs = wgs84tobd09(GPSPoint(lng, lat))
+            bdList.append((wgs.lng, wgs.lat))
+
+    with open(output, mode="w+") as f:
+        for item in bdList:
+            obj = {"longitude": item[0], "latitude": item[1]}
+            # f.write("- " + json.dumps(obj)+"\n")
+            f.write(f"- latitude: {item[1]}\n")
+            f.write(f"  longitude: {item[0]}\n")
+            # f.write(f"{item[0]},{item[1]}\n")
 
 
 if __name__ == "__main__":
@@ -117,6 +188,6 @@ if __name__ == "__main__":
     # 遍历data目录下的所有.txt的文件
     for file in os.listdir("./data"):
         if file.endswith(".txt"):
-            make_data("./data/" + file, "./output/" + file)
+            make_bd_2_84_data("./data/" + file, "./output/" + file)
 
     print("Done")
